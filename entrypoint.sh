4594a53cfb4aca7bb24abf30adc4cca1d3e74383
Fabien MARTY <121722087+fabien-marty@users.noreply.github.com>
2024-11-02 17:42:50 +0100

    add a "new-tag" output to the action

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,16 @@ set -eu
 cd /github/workspace || exit 1
 git config --global --add safe.directory '*'
 
-/app/bin/github-create-next-semantic-release .
+NEW_TAG=$(/app/bin/github-create-next-semantic-release .)
+RES=$?
+if test ${RES} -ne 0; then
+  if test ${RES} -eq 2; then
+    echo "WARNING: no release created (because no new PR found)"
+    echo "new-tag=" >> "${GITHUB_OUTPUT}"
+    exit 0
+  fi
+  echo "ERROR: failed to create release"
+  exit ${RES}
+fi
 echo "OK: release created"
+echo "new-tag=${NEW_TAG}" >> "${GITHUB_OUTPUT}"
