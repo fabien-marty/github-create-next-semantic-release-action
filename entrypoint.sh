7c0d6b437337c50c9dea93e5ed4072e0a80e2030
Fabien MARTY <fabien.marty@botify.com>
2024-11-02 09:34:19 +0100

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,4 @@ set -eu
 cd /github/workspace || exit 1
 git config --global --add safe.directory '*'
 
-TMP=$(/app/bin/github-next-semantic-version .)
-echo "${TMP}" |grep '=>' 2>/dev/null || ( echo "FAILED"; exit 1 )
-
-BEFORE=$(echo "${TMP}" |awk -F ' => ' '{print $1;}')
-AFTER=$(echo "${TMP}" |awk -F ' => ' '{print $2;}')
-
-echo "latest-version=${BEFORE}"
-echo "next-version=${AFTER}"
-if test "${GITHUB_OUTPUT:-}" != ""; then
-  echo "latest-version=${BEFORE}" >> ${GITHUB_OUTPUT}
-  echo "next-version=${AFTER}" >> ${GITHUB_OUTPUT}
-fi
+exec /app/bin/github-create-next-semantic-release .
