3382052435448eb5d08475c3b85b7925149fa138
Fabien MARTY <fabien.marty@botify.com>
2024-11-02 09:36:46 +0100

    add some debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,5 @@ set -eu
 cd /github/workspace || exit 1
 git config --global --add safe.directory '*'
 
-exec /app/bin/github-create-next-semantic-release .
+/app/bin/github-create-next-semantic-release .
+echo "OK: release created"
